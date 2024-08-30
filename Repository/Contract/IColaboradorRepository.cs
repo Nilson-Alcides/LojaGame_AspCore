59aaaf52073a28a1a7fe3a91e43a53647f84af0d
Nilson-Alcides <nilson_alcides@hotmail.com>
2024-08-30 11:41:37 -0300

    Adicionando a interface Colaborador

---
+++ Repository/Contract/IColaboradorRepository.cs
@@ -0,0 +1,14 @@
+﻿using LojaGame_AspCore.Models;
+
+namespace LojaGame_AspCore.Repository.Contract
+{
+    public interface IColaboradorRepository
+    {
+        //CRUD
+        IEnumerable<Colaborador> ObterTodosColaboradores();
+        void Cadastrar(Colaborador colaborador);
+        void Atualizar(Colaborador colaborador);
+        Colaborador ObterColaboradores(int Id);
+        void Excluir(int Id);
+    }
+}
