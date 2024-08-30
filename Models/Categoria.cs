d55028c8f0f3b910d4b409d0467e08038e0308a9
Nilson-Alcides <nilson_alcides@hotmail.com>
2024-08-30 11:13:33 -0300

    Inserrindo categoria

---
+++ Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LojaGame_AspCore.Models
+{
+    public class Categoria
+    {
+        [Display(Name = "Código", Description = "Código.")]
+        public int IdCategoria { get; set; }
+
+        [Display(Name = "Categoria", Description = "Categoria")]
+        [Required(ErrorMessage = "O nome da categoria é obrigatório.")]
+        public string NomeCategoria { get; set; }
+    }
+}
--- obj/Debug/net8.0/LojaGame_AspCore.AssemblyInfo.cs
+++ obj/Debug/net8.0/LojaGame_AspCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LojaGame_AspCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b92426e280b64c92632dbc0811542074bedc2f7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LojaGame_AspCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LojaGame_AspCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
